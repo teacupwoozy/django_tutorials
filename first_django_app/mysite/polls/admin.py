2dffa915d995fea0f69f63afa4fe5b423b8c3656
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-11-15 11:42:00 -0600

    change choice display and options in admin

--- first_django_app/mysite/polls/admin.py
+++ first_django_app/mysite/polls/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldset = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 # Alternate:
 # class QuestionAdmin(admin.ModelAdmin):
 #     fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
