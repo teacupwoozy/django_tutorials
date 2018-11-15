6b87e1c1eb054c90edc75bde23f1ff0d310c1fb1
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-11-15 16:10:47 -0600

    add admin

--- django_girls/blog/models.py
+++ django_girls/blog/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-	author = models.ForeignKey(settings.AUTH_USER_MODEL, on-delete=models.CASCADE)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	created_date = models.DateTimeField(defaul=timezone.now)
+	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 
 	def publish(self):
@@ -15,3 +15,4 @@ class Post(models.Model):
 		self.save()
 
 	def __str__(self):
+		return self.title
